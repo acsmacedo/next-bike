7edddc3e94f629f9f2dc3537be3dd040edfb5088
Anderson Macedo <andy.carlos.souza@hotmail.com>
2021-11-20 08:05:52 -0300

    Inicia projeto MVC

---
+++ NextBike/NextBike/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace NextBike.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
